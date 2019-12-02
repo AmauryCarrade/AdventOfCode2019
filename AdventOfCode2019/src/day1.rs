afaaf51bf47c6f429cacfa7ff98d116a003234aa
Amaury Carrade <amaury@carrade.eu>
2019-12-03 00:25:45 +0100

    Code formatting

--- AdventOfCode2019/src/day1.rs
+++ AdventOfCode2019/src/day1.rs
@@ -1,14 +1,21 @@
 use crate::input;
 
 pub fn run() {
-    let input: Vec<i32> = input(1, false).iter()
+    let input: Vec<i32> = input(1, false)
+        .iter()
         .map(|mass_str| mass_str.parse::<i32>().expect("Invalid number in input!"))
         .collect();
 
     let fuel_mass: i32 = input.iter().map(|m| *m).map(compute_fuel_naive).sum();
-    let real_fuel_mass: i32 = input.iter()
-        .map(|mass| std::iter::successors(Some(compute_fuel_naive(*mass)), |mass| Some(compute_fuel_naive(*mass)))
-            .take_while(|mass| *mass > 0).sum::<i32>())
+    let real_fuel_mass: i32 = input
+        .iter()
+        .map(|mass| {
+            std::iter::successors(Some(compute_fuel_naive(*mass)), |mass| {
+                Some(compute_fuel_naive(*mass))
+            })
+            .take_while(|mass| *mass > 0)
+            .sum::<i32>()
+        })
         .sum();
 
     println!("[1] Naive fuel mass: {mass}", mass = fuel_mass);
